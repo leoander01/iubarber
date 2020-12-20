f807f9d7a78983675a53236005064fcb11bfb992
Leoander Neves <44913085+leoander01@users.noreply.github.com>
2020-12-20 15:22:17 -0300

    Cadastro de Agendamentos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import routes from './routes/index';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World' });
