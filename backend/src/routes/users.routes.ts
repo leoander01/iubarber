042449f180f6a650397c6a21c1d1a0cfae9e7e2d
Leoander Neves <44913085+leoander01@users.noreply.github.com>
2021-01-10 11:19:41 -0300

    Criptografia de senha

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -14,7 +14,9 @@ usersRouter.post('/', async (request, response) => {
       name, email, password
     });
 
-    return response.send();
+    delete user.password;
+
+    return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
