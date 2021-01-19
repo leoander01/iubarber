195c0ffc5f9f7dc0d9044b8bc8d1ce928a629111
Leoander Neves <44913085+leoander01@users.noreply.github.com>
2021-01-18 21:50:13 -0300

    Gerando token JWT

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'aaa', {
+      subject: user.id,
+      expiresIn: '3d',
+    });
+
     return {
       user,
+      token,
     }
   };
 }
