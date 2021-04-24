33f0c72bbdd99548eb1d23532e48aaf5df9774ac
Leoander Neves <44913085+leoander01@users.noreply.github.com>
2021-04-23 22:12:40 -0300

    Personalizando para produção

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    }
+  }
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'leo@teste.com.br',
+      name: 'Leo do IuBarber'
+    }
+  }
+} as IMailConfig;
